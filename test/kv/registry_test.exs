d2403943439c3f89b8858774e986836f4be54339
Fred Wang <namelos@outlook.com>
2016-12-24 22:45:08 +0800

    remove process when gen server stoped

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -10,9 +10,16 @@ defmodule KV.RegistryTest do
     assert KV.Registry.lookup(registry, "shopping") == :error
 
     KV.Registry.create(registry, "shopping")
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "shoping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "remove bucket on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
